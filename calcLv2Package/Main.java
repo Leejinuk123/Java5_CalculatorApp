a81f6b5139135a491036a5a25b838f0ec6f2ca65
leejinuk <leejinouk123@kakao.com>
2024-04-24 23:31:21 +0900

    Calculator Class로 분리해서 연산과 결과값 저장하게 끔 구현

---
+++ calcLv2Package/Main.java
@@ -0,0 +1,13 @@
+package calcLv2Package;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc1 = new Calculator();
+        Scanner sc = new Scanner(System.in);
+        calc1.calculate(sc);
+
+
+    }
+}
