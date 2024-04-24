eb82088d1cea0d53864ec2e3320b24d7582e22e9
leejinuk <leejinouk123@kakao.com>
2024-04-24 23:45:52 +0900

    기능을 추려내서 빼고, 필드를 private로 변경. 게터세터 생성.

--- calcLv2Package/Calculator.java
+++ calcLv2Package/Calculator.java
@@ -1,23 +1,25 @@
 package calcLv2Package;
 
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.SimpleTimeZone;
 
 public class Calculator {
-    public ArrayList<Integer> resultList = new ArrayList<>();
+    private final ArrayList<Integer> resultList = new ArrayList<>();
 
-    public Integer calculate(Scanner sc) {
+    public ArrayList<Integer> getResultList() {
+        return resultList;
+    }
+
+    public void setResultList(int result) {
+        resultList.add(result);
+    }
+
+    public void removeResult(){
+        System.out.println("정상적으로 삭제했습니다.");
+        this.resultList.remove(0);
+    }
+
+    public Integer calculate(int a, int b, char op) {
         int result = 0;
-        String command = "";
-        do {
-            System.out.println("첫 번째 양의 정수를 입력하세요.");
-            int a = sc.nextInt();
-            System.out.println("두 번째 양의 정수를 입력하세요.");
-            int b = sc.nextInt();
-            System.out.println("사칙연산 기호를 입력하세요.");
-            char op = sc.next().charAt(0);
-
             switch (op) {
                 case '+':
                     result = a + b;
@@ -39,24 +41,6 @@ public class Calculator {
                     System.out.println("Error");
                     return 0;
             }
-
-            System.out.println("결과: " + result);
-            resultList.add(result);
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? ( remove 입력 )");
-            sc.nextLine();
-            command = sc.nextLine();
-
-            if (command.equals("remove")) resultList.remove(0);
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            command = sc.nextLine();
-
-            if (command.equals("inquiry")) System.out.println("연산 결과: " + resultList);
-
-            System.out.println("더 계산을 하겠습니까? (exit 입력 시 종료)");
-            command = sc.nextLine();
-        } while (!command.equals("exit"));
 
         return result;
     }
--- calcLv2Package/Main.java
+++ calcLv2Package/Main.java
@@ -6,8 +6,40 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc1 = new Calculator();
         Scanner sc = new Scanner(System.in);
-        calc1.calculate(sc);
+        String command = "";
+        int a,b;
+        char op;
+        int result = 0;
 
+        do {
+            System.out.println("첫 번째 양의 정수를 입력하세요.");
+            a = sc.nextInt();
+            System.out.println("두 번째 양의 정수를 입력하세요.");
+            b = sc.nextInt();
+            System.out.println("사칙연산 기호를 입력하세요.");
+            op = sc.next().charAt(0);
+
+            result = calc1.calculate(a,b,op);
+
+            System.out.println("결과: " + result);
+            calc1.setResultList(result);
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? ( remove 입력 )");
+            sc.nextLine();
+            command = sc.nextLine();
+
+            if (command.equals("remove")) calc1.removeResult();
+
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            command = sc.nextLine();
+
+            if (command.equals("inquiry")) {
+                System.out.println("연산 결과: " + calc1.getResultList());
+            }
+
+            System.out.println("더 계산을 하겠습니까? (exit 입력 시 종료)");
+            command = sc.nextLine();
+        } while (!command.equals("exit"));
 
     }
 }
